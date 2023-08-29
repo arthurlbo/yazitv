547cafb016bbe3bca3baac6aef651377a42de0bc
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2023-08-29 17:20:57 -0300

    docs: change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,9 @@ import { MainNav } from "@/components";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-poppins" });
 
 export const metadata = {
-    title: "arthurlbo | Next TypeScript Template",
-    description: "A simple Next template using Next, Typescript, Tailwind, Cypress and other tools!",
+    title: "Yazy.tv | Home",
+    description:
+        "Watch movies and TV shows together with friends and family with Yazi.tv | Stream thousands of movies and TV shows on demand, Watch with friends and family in real time, Create watch parties and chat with your friends while you watch, Get personalized recommendations based on your watch history, Download movies and TV shows to watch offline.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
