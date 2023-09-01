97576039ed7c28c04fd1a2cae400209c138dfcc2
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2023-09-01 17:34:00 -0300

    feat: adding animation

--- src/components/main-nav/cta-card.tsx
+++ src/components/main-nav/cta-card.tsx
@@ -1,46 +1,51 @@
-"use client";
-
 import Image from "next/image";
-import { useMotionValue, useTransform, motion } from "framer-motion";
 
 import GreenShepre from "@/assets/greenSphere.webp";
 import PurpleShepre from "@/assets/purpleSphere.webp";
 
 export const CtaCard = () => {
-    const x = useMotionValue(0);
-    const y = useMotionValue(0);
-    const rotateX = useTransform(y, [-100, 100], [30, -30]);
-    const rotateY = useTransform(x, [100, -100], [-30, 30]);
-
     return (
-        <motion.div
-            style={{ x, y, rotateX, perspective: "100px", rotateY, z: 100 }}
-            onMouseMove={(event) => {
-                const rect = event.currentTarget.getBoundingClientRect();
-                const offsetX = event.clientX - rect.left - rect.width / 2;
-                const offsetY = event.clientY - rect.top - rect.height / 2;
-                x.set(offsetX * 0.1);
-                y.set(offsetY * 0.1);
-            }}
-            onMouseLeave={() => {
-                x.set(0);
-                y.set(0);
-            }}
-            className="relative h-full max-h-40 w-full flex-none overflow-hidden rounded-2xl border border-hover lg:h-36 lg:max-h-36 xl:h-44 xl:max-h-44"
+        <div
+            tabIndex={0}
+            className="
+            group
+            relative
+            h-full
+            max-h-40
+            w-full
+            flex-none
+            cursor-pointer
+            overflow-hidden
+            rounded-2xl
+            border
+            border-hover
+            transition-all
+            duration-200
+            hover:border-tertiary
+            focus:outline-none
+            focus:ring-2
+            focus:ring-complementary/80
+            focus:ring-offset-2
+            focus:ring-offset-background
+            lg:h-36
+            lg:max-h-36
+            xl:h-44
+            xl:max-h-44
+            "
         >
             <Image
                 src={GreenShepre}
                 alt="green sphere"
                 priority
                 width={188}
-                className="absolute -left-24 -top-24 z-0 h-auto"
+                className="infinite absolute -left-24 -top-24 z-0 h-auto rotate-180 transition-all duration-1000 group-hover:rotate-[540deg]"
             />
             <Image
                 src={PurpleShepre}
                 alt="purple sphere"
                 priority
                 width={188}
-                className="absolute -bottom-[90px] -right-[90px] z-0 h-auto"
+                className="infinite absolute -bottom-[90px] -right-[90px] z-0  h-auto rotate-180  duration-1000 group-hover:rotate-[540deg]"
             />
             <div className="flex h-full flex-col items-center justify-center gap-4">
                 <h1 className="z-10 text-xl font-extrabold text-primary">Explore a new world !</h1>
@@ -48,6 +53,6 @@ export const CtaCard = () => {
                     Upgrade for Personalized Content & Features
                 </p>
             </div>
-        </motion.div>
+        </div>
     );
 };
