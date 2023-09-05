1d05ca876fb65a1186bcac91936c1ee38d1aff70
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2023-09-05 15:27:09 -0300

    feat: adding desktop header

---
+++ src/components/content/header.tsx
@@ -0,0 +1,44 @@
+import { Bell, MessageCircle, Search } from "lucide-react";
+
+import { Avatar } from "../ui/avatar";
+import { IconButton } from "../ui/icon-button";
+
+const SearchInput = () => {
+    return (
+        <div className="relative">
+            <Search className="absolute left-5 top-1/2 h-5 w-5 -translate-y-[52%] text-primary" />
+            <input
+                type="text"
+                placeholder="Search everything"
+                className="
+                    h-12
+                    w-[330px]
+                    rounded-full
+                    border
+                    border-hover
+                    bg-background
+                    pl-[52px]
+                    text-secondary
+                    placeholder:text-tertiary
+                    hover:border-[#545b6b]
+                    focus:border-secondary
+                    focus:outline-none"
+            />
+        </div>
+    );
+};
+
+export const Header = () => {
+    return (
+        <header className="hidden h-16 w-full items-center justify-between lg:flex">
+            <SearchInput />
+            <div className="flex items-center gap-9">
+                <div className="flex items-center gap-3">
+                    <IconButton icon={MessageCircle} />
+                    <IconButton icon={Bell} haveNotification />
+                </div>
+                <Avatar />
+            </div>
+        </header>
+    );
+};
