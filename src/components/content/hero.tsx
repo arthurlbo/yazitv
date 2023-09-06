8f4886c728afcd5813bc95ce33cab971abfd08d4
arthurlbo <arthurlbo16@gmail.com>
2023-09-05 22:15:10 -0300

    refactor: adding hover effects

--- src/components/content/hero.tsx
+++ src/components/content/hero.tsx
@@ -20,13 +20,55 @@ export const Hero = () => {
                     <Image src={Movies} alt="movies" priority className="h-[550px] w-full object-cover" />
                 </div>
                 <div className="flex w-full flex-col items-center justify-center gap-3 lg:flex-row lg:justify-start">
-                    <button className="flex w-full items-center justify-center gap-3 rounded-full bg-gradient-to-r from-accent to-complementary py-3 lg:w-64">
+                    <button
+                        className="
+                            flex
+                            w-full
+                            items-center
+                            justify-center
+                            gap-3
+                            rounded-full
+                            bg-gradient-to-r
+                            from-accent
+                            to-complementary
+                            py-3
+                            transition-all
+                            duration-300
+                            hover:scale-[1.02]
+                            hover:shadow-lg
+                            hover:shadow-complementary/30
+                            focus:outline-none
+                            focus:ring-2
+                            focus:ring-complementary/80
+                            focus:ring-offset-2
+                            focus:ring-offset-background
+                            lg:w-64"
+                    >
                         <Play className="h-6 w-6 fill-primary text-primary" />
                         <span className="text-base font-bold text-primary">Start Now</span>
                     </button>
-                    <button className="flex w-full items-center justify-center gap-1 bg-transparent py-3 lg:w-32">
-                        <span className="text-sm font-bold text-secondary">Go to My List</span>
-                        <ArrowRight className="h-5 w-5 text-secondary" />
+                    <button
+                        className="
+                            group
+                            flex
+                            w-full
+                            items-center
+                            justify-center
+                            gap-1
+                            rounded-full
+                            bg-transparent
+                            py-3
+                            focus:outline-none
+                            focus:ring-2
+                            focus:ring-complementary/80
+                            focus:ring-offset-2
+                            focus:ring-offset-background
+                            lg:w-32"
+                    >
+                        <span className="text-sm font-bold text-secondary transition-opacity duration-300 group-hover:text-primary">
+                            Go to My List
+                        </span>
+                        <ArrowRight className="h-5 w-5 text-secondary transition-transform duration-300 group-hover:translate-x-1 group-hover:text-primary" />
                     </button>
                 </div>
             </div>
