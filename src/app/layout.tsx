3427a71a5125c00fe43c0bd02014762482b94fda
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2023-09-14 17:17:07 -0300

    docs: change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700
 export const metadata = {
     title: "Yazy.tv | Home",
     description:
-        "Watch movies and TV shows together with friends and family with Yazi.tv | Stream thousands of movies and TV shows on demand, Watch with friends and family in real time, Create watch parties and chat with your friends while you watch, Get personalized recommendations based on your watch history, Download movies and TV shows to watch offline.",
+        "Watch movies and TV shows together with friends and family with Yazi.tv | Stream thousands of movies and TV shows on demand | Watch with friends and family in real time | Create watch parties and chat with your friends while you watch | Get personalized recommendations based on your watch history | Download movies and TV shows to watch offline.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
